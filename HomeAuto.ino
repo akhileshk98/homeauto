2f494847eff1fb8080976624e3664ce1a195ca82
Akhilesh K <vkakhilesh1234@gmail.com>
2019-08-10 23:31:44 +0530

    initial commit

---
+++ HomeAuto.ino
@@ -0,0 +1,15 @@
+void setup() {
+  pinMode(1,OUTPUT);
+}
+
+void loop()
+  {
+  if (Serial.available())
+  {
+    char ch = Serial.read();
+    if(ch=='A')
+    {
+      digitalWrite(1,HIGH);
+    }
+  }
+}
