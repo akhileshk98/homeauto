d39189a8bd1b6a876db6d91dece5c23b80f047d7
Akhilesh K <vkakhilesh1234@gmail.com>
2019-08-11 10:44:18 +0530

    complete code changed

--- HomeAuto.ino
+++ HomeAuto.ino
@@ -1,15 +1,34 @@
-void setup() {
-  pinMode(1,OUTPUT);
+#define LDRpin A1
+int LDRValue = 0;
+int irval=HIGH;
+int ir=7;
+void setup()
+{
+   Serial.begin(9600);
+   pinMode(13, OUTPUT);
+   pinMode(10,INPUT);
+   pinMode(irval,INPUT);
+ 
+
 }
 
 void loop()
+{
+  
+  LDRValue = analogRead(LDRpin); 
+  Serial.println(LDRValue);
+  irval=digitalRead(ir);
+  Serial.println(irval);
+  if (LDRValue<=265 && irval==LOW)
   {
-  if (Serial.available())
-  {
-    char ch = Serial.read();
-    if(ch=='A')
-    {
-      digitalWrite(1,LOW);
-    }
+    digitalWrite(13, HIGH);
+   
   }
+  else
+  {
+   digitalWrite(13, LOW);
+    
+  }
+  delay(1000);
+
 }
